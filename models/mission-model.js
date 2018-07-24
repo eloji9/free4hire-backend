e47e765199afe12ee8c1d944a886aede47cb877f
eloji9 <eloji9@hotmail.com>
2018-07-24 15:21:09 +0200

    guard and part backend search

--- models/mission-model.js
+++ models/mission-model.js
@@ -8,28 +8,26 @@ const missionSchema = new Schema({
         enum: ['cleaner', 'babysitter', 'petsitter', 'personnal driver'],
         required: true,
     },
-    adress: {
-        string: { type: String },
-        lat: { type: Number },
-        long: { type: Number }
+    worker: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
     },
     client: {
         type: Schema.Types.ObjectId,
         ref: "User",
     },
-    worker: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+    adress: {
+        type: String,
     },
-    startDate: {
+    startDateTime: {
         type: Date,
         required: true
     },
-    endDate: {
+    endDateTime: {
         type: Date,
         required: true
     },
-    price:{
+    priceHour:{
         type: Number,
         required: true,
     }
@@ -39,5 +37,34 @@ const missionSchema = new Schema({
 
 const Mission = mongoose.model('Mission', missionSchema);
 
+// missionSchema.findCorresponding = function findCorresponding(startDateTime, endDateTime, type){
+//     // const startQuery =
+//     const firstQuery =
+//     this.find({ startDateTime }).populate({path: 'client'});
+
+//     // const endQuery =
+//     const secondQuery =
+//     this.find({ endDateTime }).populate({path: 'client'});
+
+//     const thirdQuery =
+//     this.find({ type }).populate({path: 'client'});
+
+//     return Promise.all([firstQuery, secondQuery, thirdQuery])
+//     .then((results)=>{
+//       const [firstResults, secondResults] = results;
+
+//       const intersectionResults = [];
+
+//       firstResults.forEach((docA) => {
+//         secondResults.forEach((docB) => {
+//           if (docA._id.toString() === docB._id.toString()){
+//             intersectionResults.push(docA)
+//           }
+//         })
+//       })
+//       return intersectionResults
+//     })
+
+//   }
 
 module.exports = Mission;
--- routes/mission-router.js
+++ routes/mission-router.js
@@ -7,6 +7,95 @@ const Mission = require('../models/mission-model.js');
 
 const router = express.Router();
 
+// // GET ALL THE MISSIONS IN THE DATABASE
+// router.get("/missions", (req,res,next) => {
+//     Mission.find()
+//     .then((missionResults) => {
+//         res.json(missionResults);
+//     })
+//     .catch((err) => {
+//         next(err);
+//     });
+// });
 
+// POST A MISSION
+router.post("/missions", (req,res,next) => {
+    const {
+        type,
+        adress,
+        // client,
+        // worker,
+        startDate,
+        startHour,
+        startMin,
+        endDate,
+        endHour,
+        endMin,
+        price,
+        // comment,
+    } = req.body;
+    const startDateTime = `${startDate} ${startHour}:${startMin}:00`;
+    const endDateTime = `${endDate} ${endHour}:${endMin}:00`;
+    const priceHour = `${price}€`;
+    Mission.create({
+        type,
+        // worker: req.user._id,
+        // client:
+        startDateTime,
+        endDateTime,
+        priceHour,
+        adress,
+        // comment,
+    })
+    .then((missionDoc) => {
+        res.json(missionDoc);
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+// // GET THE CURRENT USER MISSIONS
+// router.get("/client/missions", (req,res,next) => {
+//     Mission.find({client:req.user._id})
+//     .then((missionResults) => {
+//         res.json(missionResults);
+//     })
+//     .catch((err) => {
+//         next(err);
+//     });
+// });
+
+// router.get("/worker/missions", (req,res,next) => {
+//     Mission.find({worker:req.user._id})
+//     .then((missionResults) => {
+//         res.json(missionResults);
+//     })
+//     .catch((err) => {
+//         next(err);
+//     });
+// });
+
+// // GET MISSIONS
+// router.get("/mission/find/:missionId", (req, res, next) => {
+
+//     const { missionId } = req.params;
+
+//     Mission.findById(missionId)
+//     .then((result) => {
+//       const {
+//         startDateTime,
+//         endDateTime }
+//         = result;
+
+//       return Mission.findCorresponding(startDateTime, endDateTime, type)
+//       .then((missionResults) => {
+//         res.json(missionResults);
+//       })
+//     })
+//     .catch((err) => {
+//       next(err)
+//     })
+//   })
 
 module.exports = router;
